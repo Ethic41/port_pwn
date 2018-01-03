7a314471c673935d9ab7102ee41dcf8baa97e2c5
Ethic41 <dahirmuhammad3@gmail.com>
2018-01-03 20:30:43 +0100

    commit

---
+++ response.js
@@ -0,0 +1,26 @@
+if(msg-0==2)
+    ermsg="Sorry, there was an error on the server. Try Re-submitting again";
+else
+if(msg-0==1)
+    ermsg="Sorry, there was an error during the reservation process. Please try re-submitting again";
+else
+if(msg-0==3)
+    ermsg="You have already reserved a room. Please rush to the bank and pay before it expires.";
+else
+if(msg-0==4)
+    ermsg="You have already paid for a room. Quickly print your allocation slip and clear the room with your Hall Administrator. You stand the risk of loosing the space and the money, if you do not clear it within 4 weeks of making payment.";
+else
+if(msg-0==6)
+    ermsg="You already have accommodation. You cannot reserve another room please.";
+else
+if(msg-0==5)
+    ermsg="Your allocation was recently revoked. You cannot have another accommodation this session.";
+else
+if(msg-0==7)
+    ermsg="Sorry the room you selected is already full. Try  selecting another room.";
+else
+if(msg-0==8)
+    ermsg="Sorry, The room might be blocked or there might be a problem with the server. Try Re-submitting again.";
+else
+if(msg-0==9)
+    ermsg="Sorry, another student might be on the process of making reservation on that room. Please wait for some minutes or select another room.";
